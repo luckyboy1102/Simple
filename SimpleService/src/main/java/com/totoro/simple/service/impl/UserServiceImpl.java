d5e06b6eec3dee402277075245439426c089ec3b
luckyboy1102 <chenshuai3333@sina.com>
2015-01-07 16:54:10 +0800

    add annotation

--- SimpleService/src/main/java/com/totoro/simple/service/impl/UserServiceImpl.java
+++ SimpleService/src/main/java/com/totoro/simple/service/impl/UserServiceImpl.java
@@ -2,12 +2,16 @@ package com.totoro.simple.service.impl;
 
 import com.totoro.simple.dao.inter.UserDAO;
 import com.totoro.simple.service.inter.UserService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * Created by admin on 2015/1/7.
  */
 public class UserServiceImpl implements UserService {
 
+    @Resource(name = "UserDAOImpl")
     private UserDAO userDAO;
 
     @Override
